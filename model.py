dfd9ca7c77e23dd89fcee54d6e812c92916c5b00
Mukund Kumar <sankhyan.mukund@gmail.com>
2023-08-18 23:01:36 +0530

    Removed unused import

--- model.py
+++ model.py
@@ -1,6 +1,5 @@
 import torch
 import torch.nn as nn
-import torch.nn.functional as F
 import torchvision.models as models
 from modules import ASPPModule, DecoderModule
 from torchsummary import summary
